fa63478974b6f1a3b08c240867ada78041434e04
Lauer <klauer@nsls2tesla.nsls.bnl.gov>
2014-07-28 15:33:27 -0400

    pep8

--- tpmac/info.py
+++ tpmac/info.py
@@ -8,17 +8,16 @@ import re
 
 from . import util
 
-ranges = {
-    'motor': [('%.2d' % x, [('xx', '%d' % x)]) for x in range(1, 33)],
-    'macro': [('%d' % n, [(' n ', ' %d' % n)]) for n in range(1, 3)],
-    'servo' : [('%d%d' % (m, n),
-               [('Servo IC m', 'Servo IC %d' % m),
-               ('Channel n', 'Channel %d' % n)])
-               for m in range(0, 10) for n in range(1, 5)],
-    'servo0' : [('%d0' % m,
-                [('Servo IC m', 'Servo IC %d' % m)])
-                for m in range(0, 10)],
-    }
+ranges = {'motor': [('%.2d' % x, [('xx', '%d' % x)]) for x in range(1, 33)],
+          'macro': [('%d' % n, [(' n ', ' %d' % n)]) for n in range(1, 3)],
+          'servo': [('%d%d' % (m, n),
+                    [('Servo IC m', 'Servo IC %d' % m),
+                     ('Channel n', 'Channel %d' % n)])
+                    for m in range(0, 10) for n in range(1, 5)],
+          'servo0': [('%d0' % m,
+                     [('Servo IC m', 'Servo IC %d' % m)])
+                     for m in range(0, 10)],
+          }
 
 
 def cs_range():
@@ -37,20 +36,24 @@ def cs_range():
 
 ranges['cs'] = cs_range()
 
+
 def ivar_to_int(ivar):
     ivar = ivar.upper()
     if not ivar.startswith('I'):
         raise ValueError('not I variable')
-    
+
     if '/' in ivar or '-' in ivar:
         raise ValueError('ivar range')
 
     return int(ivar[1:])
 
+
 def int_to_ivar(i):
     return 'I%d' % i
 
+
 ivars = {}
+
 
 def _eval_ivar(f, page, ivar, category, desc):
     if '/' in ivar:
@@ -61,14 +64,14 @@ def _eval_ivar(f, page, ivar, category, desc):
         r0, r1 = [ivar_to_int(iv) for iv in ivar.split('-')]
         for i in range(r0, r1 + 1):
             _eval_ivar(f, page, int_to_ivar(i), category, desc)
-    else: 
+    else:
         try:
             ivar_int = ivar_to_int(ivar)
         except:
             print('Failed: %s' % ivar)
         else:
             print('i%d\t%s\t%s\t%d' % (ivar_int, desc, category, page), file=f)
-        
+
             if ivar in ivars:
                 print('Duplicate', ivar, ivars[ivar], '//', (category, desc))
 
@@ -88,26 +91,28 @@ def eval_ivar(f, page, ivar, category, desc):
         class_, replace = 'cs', 'sx'
     else:
         class_ = ''
-    
+
     if class_:
         range_ = ranges[class_]
         for replace_with, desc_replace in range_:
             _eval_ivar(f, page,
-                      ivar.replace(replace, replace_with), 
-                      replace_multiple(category, *desc_replace),
-                      replace_multiple(desc, *desc_replace))
+                       ivar.replace(replace, replace_with),
+                       replace_multiple(category, *desc_replace),
+                       replace_multiple(desc, *desc_replace))
     else:
         _eval_ivar(f, page, ivar, category, desc)
+
 
 def replace_multiple(s, *from_to):
     for from_, to in from_to:
         s = s.replace(from_, to)
     return s
 
+
 def generate_toc_info(input_fn, output_fn, only_ivars=False):
     category = ''
     re_istart = re.compile('(I[sxmn0-9][sxmn0-9-]*)')
-    
+
     all_toc = []
 
     category = ''
@@ -122,16 +127,16 @@ def generate_toc_info(input_fn, output_fn, only_ivars=False):
                     continue
 
                 is_category = not line.startswith(' ')
-                
+
                 line, page = line.split('....', 1)
                 line = line.rstrip('.').strip()
                 page = int(page.lstrip('.'))
-                
+
                 line = line.replace('\t', ' ')
                 line = line.replace('  ', ' ')
 
                 all_toc.append((page, line))
-                
+
                 if is_category:
                     category = line
 
@@ -145,6 +150,7 @@ def generate_toc_info(input_fn, output_fn, only_ivars=False):
 
                 print('%s\t%s\t%d' % (line, category, page), file=f)
 
+
 def generate_mem_info(fn, output_fn):
     addr_info = {}
 
@@ -155,9 +161,9 @@ def generate_mem_info(fn, output_fn):
             s = 'CS %s' % s[1:]
         elif s.startswith('#'):
             s = 'Motor %s' % s[1:]
-        
+
         return s
-    
+
     for line in open(fn, 'rt').readlines():
         line = line.strip()
         if ';' in line:
@@ -169,18 +175,18 @@ def generate_mem_info(fn, output_fn):
         line = whitespace.subn('', line)[0]
         if not line:
             continue
-         
+
         if '->' in line:
             mvar, mem = line.split('->')
             if mem.strip() == '*':
                 continue
-            
+
             mvar = mvar.upper()
             mem = util.clean_addr(mem)
-            
+
             if not comment:
                 print('%s points to %s which is %s' % (mvar, mem, comment))
-            
+
             addr_info[mem] = fix_comment(comment)
 
     with open(output_fn, 'wt') as f:
@@ -200,18 +206,18 @@ class Info(object):
                 continue
 
             info = line.split(delim)
-            
+
             key = info[0]
             self._lower_keys[key.lower()] = key
             data[key] = info[1:]
-    
+
     def __getitem__(self, key):
         if key in self._lower_keys:
             key = self._lower_keys[key]
 
         return self.data[key]
 
-    def search(self, text, in_keys=True, in_data=True, 
+    def search(self, text, in_keys=True, in_data=True,
                case_insensitive=True):
         # note: very inefficient search
         if case_insensitive:
@@ -250,10 +256,11 @@ ivar_info = None
 mem_info = None
 toc_info = None
 
+
 def load_settings(profile, ivar_fn=IVAR_FN, mem_fn=MEM_FN,
-                 toc_fn=TOC_FN):
+                  toc_fn=TOC_FN):
     global ivar_info, mem_info, toc_info
-    
+
     profile_path = util.get_profile_path(profile)
 
     ivar_fn = os.path.join(profile_path, ivar_fn)
@@ -264,6 +271,7 @@ def load_settings(profile, ivar_fn=IVAR_FN, mem_fn=MEM_FN,
 
     toc_fn = os.path.join(profile_path, toc_fn)
     toc_info = Info(toc_fn)
+
 
 def generate_settings(profile, ivar_fns=(RAW_TOC_FN, IVAR_FN),
                       mem_fns=(RAW_MEM_FN, MEM_FN),
@@ -277,12 +285,12 @@ def generate_settings(profile, ivar_fns=(RAW_TOC_FN, IVAR_FN),
     generate_toc_info(ivar_fns[0], ivar_fns[1], only_ivars=True)
     generate_mem_info(*mem_fns)
     generate_toc_info(*toc_fns)
-    
-    
+
+
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: %s profile_name' % (sys.argv[0]))
         sys.exit(1)
-    
+
     profile = sys.argv[1]
     generate_settings(profile)
